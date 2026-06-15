13e9eb174fbbe98d2350657f4eb283bd80598b04
coder-apr-5 <apurbaroy285@gmail.com>
2026-06-15 21:51:44 +0530

    chore: remove obsolete update script used for one-time database migration

--- update.py
+++
@@ -1,9 +0,0 @@
-from utils.db import init_db
-import sqlite3
-
-# Ensure schema is up to date
-init_db()
-
-c = sqlite3.connect('database.db')
-c.execute("UPDATE users SET display_name='Apurba Roy' WHERE email='apurbaroyleo5@gmail.com'")
-c.commit()
